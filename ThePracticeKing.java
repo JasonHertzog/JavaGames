4dce7bee4018afe62af7ff21a04964145750e3ce
Jason Hertzog <86388948+JasonHertzog@users.noreply.github.com>
2022-01-18 21:17:42 -0500

    Created initial idea
    Initial idea created. No work started.

---
+++ ThePracticeKing.java
@@ -0,0 +1,21 @@
+/*
+ * "ThePracticeKing" is a game that will reward the king with
+ * land and treasures for each day that some type of programming
+ * progress has been made.
+ * 
+ * [TO-DO]
+ * Gold System
+ * 
+ * [IDEAS]
+ * Land System
+ * Reward System
+ * Streak Rewards
+ * 
+ */
+
+public class ThePracticeKing {
+    public static void main(String[] args) {
+
+        System.out.println("\nWelcome King!\n");
+    }
+}

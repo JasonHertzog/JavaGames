3373701625694117675e0f3c61dbdb13bcf0fa73
JasonHertzog <86388948+JasonHertzog@users.noreply.github.com>
2022-02-26 14:41:06 -0500

    Update ThePracticeKing.java
    Minor update

--- ThePracticeKing.java
+++ ThePracticeKing.java
@@ -4,6 +4,7 @@
  * progress has been made.
  * 
  * [TO-DO]
+ * Saving System
  * Gold System
  * 
  * [IDEAS]
@@ -11,11 +12,41 @@
  * Reward System
  * Streak Rewards
  * 
+ * 
  */
+import java.io.Serializable; // Used for save system.
+import java.util.Scanner; // Used for inputs to update game state.
 
 public class ThePracticeKing {
     public static void main(String[] args) {
-
-        System.out.println("\nWelcome King!\n");
+    
+        int gold = 0; // Start at 0 gold.
+        char intent; // This will be used to determine the next action to take.         
+        
+        char[] intentMenu = { // Game menu initialization.
+            'm', 's'
+        }; // m = Menu options  ;   s = Save game
+        
+        Scanner scnr = new Scanner(System.in);
+        
+        System.out.println("\nWelcome King!\nYou have " + gold + " coins!\n");
+        System.out.print("What would you like to do next? (Enter \"M\" for a menu.) ");
+        
+        // ############################################
+        // WE WILL NEED A LOOP HERE FOR OBVIOUS REASONS
+        // ############################################
+        
+        intent = Character.toLowerCase(scnr.next().charAt(0)); // Figure out what the user wants to do next.
+        
+        if (intent == 'm') {
+            System.out.println("We currently only have two options, and one doesn't even work. \nM = Menu \nS = Save (Being worked on)");
+        } 
+        else if (intent == 's') {
+            System.out.println("If it was working, we'd be saving right now.");
+        }
+        else {
+            System.out.println("Something went wrong!");
+        }
+        
     }
 }
